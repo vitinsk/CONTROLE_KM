afa2fe8c03ecc6b0c39f118f0f32dda32d97fcdd
vitinsk <vitinsk@hotmail.com>
2018-03-26 22:13:58 -0300

    diego gay

--- views/home.php
+++ views/home.php
@@ -5,16 +5,14 @@
 <h1 style="padding-left: 200px;" > alteração do vitor</h1>
 
 
-<<<<<<< HEAD
+
 <h1 style="padding-left: 300px;"> OI HIGOR</h1>
 
-=======
-<input type="text" style="padding-left: 500px" name="" value="Vitor">
 
-<h1 style="padding-left: 200px;" > HIIIIIIIIIIIIGOROOOOOOOORRRRRRRR</h1>
 
-<h1 style="padding-left: 200px;" > vitu/h1>
->>>>>>> 76ee53dd53682a3a0a52bf638a8b374a0b050246
+<h1 style="padding-left: 200px;" > diego gay</h1>
 
-<h1 style="padding-left: 200px;" > higooooooooooooooooooooorrrrrrrrrr</h1>
+
+
+
 
