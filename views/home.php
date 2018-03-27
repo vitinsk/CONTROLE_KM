6adbe61cc418d26fc87d3752e1f2d1ef876ed157
vitinsk <vitinsk@hotmail.com>
2018-03-26 21:38:13 -0300

    vitor

--- views/home.php
+++ views/home.php
@@ -1,10 +1,7 @@
 <?php echo $menssagem ?>
 
 
-<<<<<<< HEAD
-<h1 style="padding-left: 200px;" > novo teste no notebook do Igor ad sadasdas</h1>
-<h1> Testando Igor </h1>
-<h1 style="padding-left: 200px;" > novo teste no notebook do Igor ad sadasdas</h1>
-<h1> Testando Igor </h1>
+
+<h1 style="padding-left: 200px;" > alteração do vitor</h1>
 
 
